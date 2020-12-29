bbb4e2cde8bb4171b2229977ffbf19750e652c64
karaage0703 <coldchicken@gmail.com>
2020-12-29 17:30:04 +0900

    Add control arm function

--- mycobot_control.py
+++ mycobot_control.py
@@ -2,14 +2,16 @@ import argparse
 import serial
 import time
 
-HEADER = 'FA'
-FOOTER = 'FE'
 BAUDRATE = 115200
 
+HEADER = 'FA'
+FOOTER = 'FE'
+
+ANGLE_TO_ENCODER = 11.3777777
+ENCODER_CAL_POINT = 2048
+
 parser = argparse.ArgumentParser(description='mycobot_control')
-
 parser.add_argument('serial_name', help='serial name')
-
 args = parser.parse_args()
 
 ser = serial.Serial(args.serial_name, BAUDRATE, timeout=0.1)
@@ -27,13 +29,14 @@ def set_rgb(rgb):
     ser.write(bytes.fromhex(command))
 
 
-def set_angle(servo_no, angle, speed):
-    """Set the robot angle
+def _convert_servo_encoder_value(angle):
+    """Convert angle(degree) to servo encoder value
 
     Args:
-        servo_no (str): '01' to '06'
-        angle (int): -180 to 180
-        speed (str): '0000' to ?
+        angle (int): -180 to 180 (degree)
+
+    Returns:
+        str: servo encoder value
     """
 
     ANGLE_TO_ENCODER = 11.3777777
@@ -48,8 +51,54 @@ def set_angle(servo_no, angle, speed):
     servo_encoder_value = int(angle * ANGLE_TO_ENCODER) + ENCODER_CAL_POINT
     servo_encoder_value = '%04X' % servo_encoder_value
 
+    return servo_encoder_value
+
+
+def set_angle(servo_no, angle, speed):
+    """Set the robot angle
+
+    Args:
+        servo_no (str): '01' to '06'
+        angle (int): -180 to 180 (degree)
+        speed (str): '0000' to ?
+    """
+
+    servo_encoder_value = _convert_servo_encoder_value(angle)
+
     command = (
         HEADER + HEADER + '07' + '20' +
         servo_no + servo_encoder_value[2:4] + servo_encoder_value[0:2] +
         speed[2:4] + speed[0:2] + FOOTER)
+
     ser.write(bytes.fromhex(command))
+
+
+def set_angles(angles, speed):
+    """Set the robot angle
+
+    Args:
+        angles (list): example[10, 180, -30, 0, 10, 20] (degree)
+        speed (str): '0000' to ?
+    """
+
+    if len(angles) != 6:
+        raise Exception('Error!')
+    else:
+        servo_encoder_values = []
+        for angle in angles:
+            servo_encoder_values.append(_convert_servo_encoder_value(angle))
+
+        command = HEADER + HEADER + '10' + '21'
+        for servo_encoder_value in servo_encoder_values:
+            command += servo_encoder_value[2:4] + servo_encoder_value[0:2]
+        command += speed[2:4] + speed[0:2] + FOOTER
+
+        ser.write(bytes.fromhex(command))
+
+
+# Test commands:
+# python -i mycobot_control.py '/dev/tty.SLAB_USBtoUART'
+# >>> set_rgb('FF0000') # red
+# >>> set_angle('01', 0, '0005')
+# >>> set_angle('04', 0, '0005')
+# set_angles([0,10,200,-180,-200,0], '0005')
